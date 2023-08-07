8de224ada5a5ebb24af308a67fc3a571ee3600df
Mark <42696773+MarkHmnv@users.noreply.github.com>
2023-08-07 16:37:22 +0300

    Init service (#1)
    * Init service
    
    * Add RabbitMQ configuration
    
    The configuration includes the setup to convert messages into JSON format using Jackson, ensuring appropriate handling of dates and timestamps.
    
    * Move main application class to correct package

---
+++ src/main/java/io/github/uptalent/notification/config/RabbitMQConfig.java
@@ -0,0 +1,21 @@
+package io.github.uptalent.notification.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public MessageConverter messageConverter() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return new Jackson2JsonMessageConverter(mapper);
+    }
+}
